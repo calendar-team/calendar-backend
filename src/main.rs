8f8e83e7b87b80396ce195a87a3475f9655ee1eb
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-09-03 12:46:50 +0300

    Clean dead code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use std::{fs::File, io::BufReader};
 use actix_cors::Cors;
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer};
 use log::info;
-use std::env;
 use rusqlite::Connection;
-use std::sync::{Arc, Mutex};
-use tide::http::headers::HeaderValue;
-use tide::prelude::*;
-use tide::security::{CorsMiddleware, Origin};
-use tide::{Request, Response, StatusCode};
-use actix_web::{middleware::Logger, http, post, get, web, App, HttpServer, HttpResponse, Responder, Result};
 use rustls::{Certificate, PrivateKey, ServerConfig};
 use rustls_pemfile::{certs, pkcs8_private_keys};
+use serde::Deserialize;
+use serde::Serialize;
+use std::env;
+use std::sync::{Arc, Mutex};
+use std::{fs::File, io::BufReader};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Event {
@@ -32,41 +30,45 @@ struct State {
 }
 
 #[post("/event")]
-async fn create_event_2(event: web::Json<Event>, state: web::Data<State>) -> HttpResponse {
-            let mut stmt_result = (&state).conn.lock().expect("failed to lock conn");
-            let conn = &mut *stmt_result;
+async fn create_event(event: web::Json<Event>, state: web::Data<State>) -> HttpResponse {
+    let mut stmt_result = (&state).conn.lock().expect("failed to lock conn");
+    let conn = &mut *stmt_result;
 
-            let result = conn.execute(
-                "INSERT INTO event (name, calendar_id, date_time) VALUES (?1, ?2, ?3)",
-                (&event.name, &event.calendar_id, &event.date_time),
-            );
-            match result {
-                Ok(_) => {
-                    info!("inserted event");
-                }
-                Err(e) => {
-                    info!("error inserting event: {}", e);
-                    return HttpResponse::InternalServerError().finish();
-                }
-            }
+    let result = conn.execute(
+        "INSERT INTO event (name, calendar_id, date_time) VALUES (?1, ?2, ?3)",
+        (&event.name, &event.calendar_id, &event.date_time),
+    );
+    match result {
+        Ok(_) => {
+            info!("inserted event");
+        }
+        Err(e) => {
+            info!("error inserting event: {}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
+    }
     HttpResponse::Created().finish()
 }
 
 #[get("/calendar/{id}")]
-async fn get_calendar_2(id: web::Path<String>, state: web::Data<State>) -> HttpResponse{
+async fn get_calendar(id: web::Path<String>, state: web::Data<State>) -> HttpResponse {
     info!("getting calendar for {}", id);
 
     let mut stmt_result = (&state).conn.lock().expect("failed to lock conn");
     let conn = &mut *stmt_result;
-    let mut stmt = conn.prepare("SELECT name, calendar_id, date_time FROM event").unwrap();
+    let mut stmt = conn
+        .prepare("SELECT name, calendar_id, date_time FROM event")
+        .unwrap();
 
-    let event_iter = stmt.query_map([], |row| {
-        Ok(Event {
-            name: row.get(0)?,
-            calendar_id: row.get(1)?,
-            date_time: row.get(2)?,
+    let event_iter = stmt
+        .query_map([], |row| {
+            Ok(Event {
+                name: row.get(0)?,
+                calendar_id: row.get(1)?,
+                date_time: row.get(2)?,
+            })
         })
-    }).unwrap();
+        .unwrap();
 
     let mut events = Vec::new();
     for event in event_iter {
@@ -83,7 +85,6 @@ async fn get_calendar_2(id: web::Path<String>, state: web::Data<State>) -> HttpR
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let conn = Connection::open_in_memory().unwrap();
 
@@ -95,7 +96,8 @@ async fn main() -> std::io::Result<()> {
             date_time   TEXT NOT NULL
         )",
         (), // empty list of parameters.
-    ).unwrap();
+    )
+    .unwrap();
 
     let state = State {
         conn: Arc::new(Mutex::new(conn)),
@@ -108,8 +110,8 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::clone(&data))
             .wrap(Logger::default())
             .wrap(Cors::permissive())
-            .service(create_event_2)
-            .service(get_calendar_2)
+            .service(create_event)
+            .service(get_calendar)
     });
 
     if env::var("CALENDAR_USE_TLS").is_ok() {
@@ -128,8 +130,12 @@ fn load_rustls_config() -> rustls::ServerConfig {
         .with_no_client_auth();
 
     // load TLS key/cert files
-    let cert_file = &mut BufReader::new(File::open("/etc/letsencrypt/live/backend.calendar.aguzovatii.com/fullchain.pem").unwrap());
-    let key_file = &mut BufReader::new(File::open("/etc/letsencrypt/live/backend.calendar.aguzovatii.com/privkey.pem").unwrap());
+    let cert_file = &mut BufReader::new(
+        File::open("/etc/letsencrypt/live/backend.calendar.aguzovatii.com/fullchain.pem").unwrap(),
+    );
+    let key_file = &mut BufReader::new(
+        File::open("/etc/letsencrypt/live/backend.calendar.aguzovatii.com/privkey.pem").unwrap(),
+    );
 
     // convert files to key/cert objects
     let cert_chain = certs(cert_file)
@@ -150,107 +156,4 @@ fn load_rustls_config() -> rustls::ServerConfig {
     }
 
     config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
-}
-
-// #[async_std::main]
-async fn main2() -> tide::Result<()> {
-    env_logger::init();
-    info!("starting up");
-
-    let conn = Connection::open_in_memory()?;
-
-    conn.execute(
-        "CREATE TABLE event (
-            id          INTEGER PRIMARY KEY,
-            name        TEXT NOT NULL,
-            calendar_id TEXT NOT NULL,
-            date_time   TEXT NOT NULL
-        )",
-        (), // empty list of parameters.
-    )?;
-
-    let state = State {
-        conn: Arc::new(Mutex::new(conn)),
-    };
-
-    let cors = CorsMiddleware::new()
-        .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
-        .allow_origin(Origin::from("*"))
-        .allow_credentials(false);
-
-    let mut app = tide::with_state(state);
-    app.with(cors);
-
-    app.at("/calendar/:id").get(get_calendar);
-    app.at("/event").post(create_event);
-
-    if env::var("CALENDAR_USE_TLS").is_ok() {
-        app.listen(
-            tide_rustls::TlsListener::build().addrs("0.0.0.0:8080")
-            .cert("/etc/letsencrypt/live/backend.calendar.aguzovatii.com/fullchain.pem")
-            .key("/etc/letsencrypt/live/backend.calendar.aguzovatii.com/privkey.pem"),
-        )
-        .await?;
-    } else {
-        app.listen("0.0.0.0:8080").await?;
-    }
-    Ok(())
-}
-
-async fn get_calendar(req: Request<State>) -> tide::Result {
-    let id = req.param("id")?;
-    info!("getting calendar for {}", id);
-
-    let mut stmt_result = req.state().conn.lock().expect("failed to lock conn");
-    let conn = &mut *stmt_result;
-    let mut stmt = conn.prepare("SELECT name, calendar_id, date_time FROM event")?;
-
-    let event_iter = stmt.query_map([], |row| {
-        Ok(Event {
-            name: row.get(0)?,
-            calendar_id: row.get(1)?,
-            date_time: row.get(2)?,
-        })
-    })?;
-
-    let mut events = Vec::new();
-    for event in event_iter {
-        events.push(event.unwrap());
-    }
-
-    let calendar = Calendar {
-        id: id.to_string(),
-        events,
-    };
-
-    Ok(serde_json::to_string(&calendar)?.into())
-}
-
-async fn create_event(mut req: Request<State>) -> tide::Result {
-    let event: tide::Result<Event> = req.body_json().await;
-
-    match event {
-        Ok(event) => {
-            let mut stmt_result = req.state().conn.lock().expect("failed to lock conn");
-            let conn = &mut *stmt_result;
-            let result = conn.execute(
-                "INSERT INTO event (name, calendar_id, date_time) VALUES (?1, ?2, ?3)",
-                (event.name, event.calendar_id, event.date_time),
-            );
-            match result {
-                Ok(_) => {
-                    info!("inserted event");
-                }
-                Err(e) => {
-                    info!("error inserting event: {}", e);
-                    return Ok(Response::new(StatusCode::InternalServerError));
-                }
-            }
-        }
-        Err(e) => {
-            info!("error parsing event: {}", e);
-            return Ok(Response::new(StatusCode::BadRequest));
-        }
-    }
-    Ok(Response::new(StatusCode::Created))
 }
