187625ac2a924408069ae8b2da7be50bce7ac457
Diana Jacota <diana.jacota@gmail.com>
2023-09-13 15:01:56 +0300

    Implement Basic Authentication for the event endpoint
    - add a CustomError enum which will be used to represent two types of
      errors: AuthError (which occurs at authorization) and UnexpectedError
    - in method basic_authentication: retrieve the credentials from the Authorization header. If it is
      missing, return an AuthError
    - in method validate_credentials: get the userId from the db for the credentials provided by the user.
      If no userId is retrieved, return an error because the user doesn't
      exist
    - add the user_id in the event table which will be used to match the
      event to the user. The parameter username will be removed in another
      task
    - add 2 integration tests, one for the case in which the user doesn't
      provide the credentials and one in which the credentials provided are
      invalid
    - update the create_event_works() test to use valid credentials when an
      event is created

--- tests/event.rs
+++ tests/event.rs
@@ -3,20 +3,20 @@ use std::net::TcpListener;
 
 #[tokio::test]
 async fn create_event_works() {
-    // Arrange
+    // Arrange - create the user
     let address = spawn_app();
+    let username = "djacota";
+    let password = "password";
 
-    let client = reqwest::Client::new();
-    let event = serde_json::json!({
-        "username": "djacota",
-        "name": "write_integration_tests",
-        "date_time": "09-05-2023"
+    let user = serde_json::json!({
+        "username": username,
+        "password": password,
     });
 
     // Act
-    let response = client
-        .post(&format!("{}/event", &address))
-        .json(&event)
+    let response = reqwest::Client::new()
+        .post(&format!("{}/user", &address))
+        .json(&user)
         .send()
         .await
         .expect("Failed to execute request.");
@@ -24,13 +24,53 @@ async fn create_event_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+
+    // Arrange - create an event for the user previously created
+    let event = serde_json::json!({
+        "username": "djacota",
+        "name": "write_integration_tests",
+        "date_time": "09-05-2023"
+    });
+
+    // Act
+    let response_event = reqwest::Client::new()
+        .post(&format!("{}/event", &address))
+        .basic_auth(username, Some(password))
+        .json(&event)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response_event.status().is_success());
+    assert_eq!(Some(0), response_event.content_length());
 }
 
 #[tokio::test]
-async fn create_event_missing_fields_return_400_bad_request() {
-    // Arrange event without name
+async fn create_event_returns_400_when_fields_are_not_available() {
+    // Arrange - create the user
     let address = spawn_app();
+    let username = "djacota";
+    let password = "password";
 
+    let user = serde_json::json!({
+        "username": username,
+        "password": password,
+    });
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/user", &address))
+        .json(&user)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+
+    // Arrange - event without name
     let event = serde_json::json!({
         "username": "djacota",
         "calendar_id": "djacota",
@@ -40,6 +80,7 @@ async fn create_event_missing_fields_return_400_bad_request() {
     // Act
     let response = reqwest::Client::new()
         .post(&format!("{}/event", &address))
+        .basic_auth(username, Some(password))
         .json(&event)
         .send()
         .await
@@ -65,6 +106,55 @@ async fn create_event_missing_fields_return_400_bad_request() {
 
     // Assert
     assert_eq!(400, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn event_requests_missing_authorization_are_rejected() {
+    // Arrange
+    let address = spawn_app();
+
+    let event = serde_json::json!({
+        "username": "djacota",
+        "name": "implement_basic_authentication",
+        "calendar_id": "djacota",
+        "date_time": "09-05-2023"
+    });
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/event", &address))
+        .json(&event)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn event_requests_with_invalid_credentials_are_rejected() {
+    // Arrange
+    let address = spawn_app();
+
+    let event = serde_json::json!({
+        "username": "djacota",
+        "name": "implement_basic_authentication",
+        "calendar_id": "djacota",
+        "date_time": "09-05-2023"
+    });
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/event", &address))
+        .basic_auth("username", Some("password"))
+        .json(&event)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
 }
 
 // launch the server as a background task
