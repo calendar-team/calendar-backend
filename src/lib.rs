ef143cfe3d84fd3ef0e7e859023c51c45f78bceb
DianaJacota <48437464+DianaJacota@users.noreply.github.com>
2023-09-14 09:44:55 +0300

    Basic authentication for /event endpoint (#21)
    * Implement Basic Authentication for the event endpoint
    
    - add a CustomError enum which will be used to represent two types of
      errors: AuthError (which occurs at authorization) and UnexpectedError
    - in method basic_authentication: retrieve the credentials from the Authorization header. If it is
      missing, return an AuthError
    - in method validate_credentials: get the userId from the db for the credentials provided by the user.
      If no userId is retrieved, return an error because the user doesn't
      exist
    - add the user_id in the event table which will be used to match the
      event to the user. The parameter username will be removed in another
      task
    - add 2 integration tests, one for the case in which the user doesn't
      provide the credentials and one in which the credentials provided are
      invalid
    - update the create_event_works() test to use valid credentials when an
      event is created

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 use actix_cors::Cors;
 use actix_web::dev::Server;
-use actix_web::http::header;
-use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer};
+use actix_web::http::header::{self, HeaderMap};
+use actix_web::http::StatusCode;
+use actix_web::{
+    get, middleware::Logger, post, web, App, HttpRequest, HttpResponse, HttpServer, ResponseError,
+};
+use anyhow::Context;
+use base64::Engine;
 use log::info;
-use rusqlite::Connection;
+use rusqlite::{Connection, OptionalExtension};
 use rustls::{Certificate, PrivateKey, ServerConfig};
 use rustls_pemfile::{certs, pkcs8_private_keys};
+use secrecy::{ExposeSecret, Secret};
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::fmt::Debug;
 use std::net::TcpListener;
 use std::sync::{Arc, Mutex};
 use std::{fs::File, io::BufReader};
@@ -36,6 +43,29 @@ struct State {
     conn: Arc<Mutex<Connection>>,
 }
 
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum CustomError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl ResponseError for CustomError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            CustomError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            // Return a 401 for auth errors
+            CustomError::AuthError(_) => StatusCode::UNAUTHORIZED,
+        }
+    }
+}
+
 pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
     let _ = env_logger::try_init_from_env(env_logger::Env::new().default_filter_or("info"));
     let conn = Connection::open_in_memory().unwrap();
@@ -43,6 +73,7 @@ pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
     conn.execute(
         "CREATE TABLE event (
             id          INTEGER PRIMARY KEY,
+            user_id     INTEGER NOT NULL,
             username    TEXT NOT NULL,
             name        TEXT NOT NULL,
             date_time   TEXT NOT NULL
@@ -54,7 +85,7 @@ pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
     conn.execute(
         "CREATE TABLE user (
             id        INTEGER PRIMARY KEY,
-            username  TEXT NOT NULL,
+            username  TEXT NOT NULL UNIQUE,
             password  TEXT NOT NULL
         )",
         (),
@@ -102,13 +133,20 @@ pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
 }
 
 #[post("/event")]
-async fn create_event(event: web::Json<Event>, state: web::Data<State>) -> HttpResponse {
+async fn create_event(
+    req: HttpRequest,
+    event: web::Json<Event>,
+    state: web::Data<State>,
+) -> Result<HttpResponse, CustomError> {
     let mut stmt_result = (&state).conn.lock().expect("failed to lock conn");
     let conn = &mut *stmt_result;
 
+    let credentials = basic_authentication(req.headers()).map_err(CustomError::AuthError)?;
+    let user_id = validate_credentials(credentials, conn).await?;
+
     let result = conn.execute(
-        "INSERT INTO event (username, name, date_time) VALUES (?1, ?2, ?3)",
-        (&event.username, &event.name, &event.date_time),
+        "INSERT INTO event (user_id, username, name, date_time) VALUES (?1, ?2, ?3, ?4)",
+        (&user_id, &event.username, &event.name, &event.date_time),
     );
     match result {
         Ok(_) => {
@@ -116,10 +154,12 @@ async fn create_event(event: web::Json<Event>, state: web::Data<State>) -> HttpR
         }
         Err(e) => {
             info!("error inserting event: {}", e);
-            return HttpResponse::InternalServerError().finish();
+            return Err(CustomError::UnexpectedError(anyhow::anyhow!(
+                "Error when saving the event"
+            )));
         }
     }
-    HttpResponse::Created().finish()
+    Ok(HttpResponse::Created().finish())
 }
 
 #[get("/calendar/{username}")]
@@ -227,4 +267,59 @@ fn load_rustls_config() -> rustls::ServerConfig {
     }
 
     config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // The header value, if present, must be a valid UTF8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string.")?;
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_bytes = base64::engine::general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8.")?;
+    // Split into two segments, using ':' as delimiter
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
+}
+
+async fn validate_credentials(
+    credentials: Credentials,
+    conn: &Connection,
+) -> Result<i64, CustomError> {
+    let mut stmt = conn
+        .prepare("SELECT id FROM user WHERE username = ?1 and password = ?2")
+        .unwrap();
+
+    let id: Option<i64> = stmt
+        .query_row(
+            &[
+                credentials.username.as_str(),
+                credentials.password.expose_secret().as_str(),
+            ],
+            |row| row.get(0),
+        )
+        .optional()
+        .unwrap();
+
+    return id
+        .ok_or_else(|| CustomError::AuthError(anyhow::anyhow!("Invalid username or password.")));
 }
