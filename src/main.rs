b3e177192384aa06df0f037df03294526dc2383f
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2024-02-28 23:20:24 +0200

    Listen to ANY IPV6 addr

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ async fn main() -> Result<(), std::io::Error> {
     let db_path = "./database.db3";
     let conn = Connection::open(db_path).unwrap();
 
-    let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind to port 8080");
+    let listener = TcpListener::bind("[::]:8080").expect("Failed to bind to port 8080");
     run(listener, conn)?.await
 }
